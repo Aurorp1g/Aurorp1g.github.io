9852d3eb0e97ebe621af040908c972a77c499865
Aurorp1g <Ze-662607015@outlook.com>
2025-05-16 20:46:30 +0800

    Site updated: 2025-05-16 20:46:30

--- js/fomal.js
+++ js/fomal.js
@@ -73,7 +73,7 @@ $.ajax({
   type: 'get',
   url: 'https://apis.map.qq.com/ws/location/v1/ip',
   data: {
-    key: '',  // 这里要写你的KEY!!!
+    key: 'I55BZ-VAB65-FN5I4-I5OXE-XY576-KEFCB',  // 这里要写你的KEY!!!
     output: 'jsonp',
   },
   dataType: 'jsonp',
