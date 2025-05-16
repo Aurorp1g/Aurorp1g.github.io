f685a0a6621bdf4ff26b2d14875a840f4e1a337b
Aurorp1g <Ze-662607015@outlook.com>
2025-05-16 10:03:32 +0800

    Site updated: 2025-05-16 10:03:30

---
+++ js/aplayer.js
@@ -0,0 +1,59 @@
+new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    theme: '#1F1F1F',
+    listFolded: true,
+    listMaxHeight: 90,
+    lrcType: 3,
+    audio: [
+      {
+        name: '降临',
+      artist: 'Breen',
+      url: '/music/mp3/降临.mp3',
+      cover: '/music/cover/降临.jpg',
+      lrc: '/music/lrc/降临.lrc',
+      theme: '#1F1F1F'
+      },
+      {
+        name: 'off the hook',
+        artist: 'Jeff Jarvis',
+        url: '/music/mp3/off_the_hook.mp3',
+        cover: '/music/cover/off_the_hook.jpg',
+        lrc: '/music/lrc/off_the_hook.lrc',
+        theme: '#1F1F1F'
+      },
+      {
+        name: 'Follow(罗生门)',
+        artist: '梨冻紧&张子豪',
+        url: '/music/mp3/Follow(罗生门).mp3',
+        cover: '/music/cover/Follow(罗生门).jpg',
+        lrc: '/music/lrc/Follow(罗生门).lrc',
+        theme: '#1F1F1F'
+      },
+      {
+        name: 'Love With U',
+        artist: 'Tess Ortega',
+        url: '/music/mp3/love_with_U.mp3',
+        cover: '/music/cover/love_with_U.jpg',
+        lrc: '/music/lrc/love_with_U.lrc',
+        theme: '#1F1F1F'
+      },
+      {
+          name: 'Down 4u',
+        artist: 'JuggShots',
+        url: '/music/mp3/Down_4u.mp3',
+        cover: '/music/cover/Down_4u.jpg',
+        lrc: '/music/lrc/Down_4u.lrc',
+        theme: '#1F1F1F'
+      },
+      {
+          name: 'boyfriend (Explicit)',
+        artist: 'Ariana Grande、Social House',
+        url: '/music/mp3/boyfriend.mp3',
+        cover: '/music/cover/boyfriend.jpg',
+        lrc: '/music/lrc/boyfriend.lrc',
+        theme: '#1F1F1F'
+      }
+    ]
+  });
